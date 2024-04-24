4a6ddaf06a64f498fad0d5ea30cbc22599a1b784
Maksym <suzdalcevmaksym@gmail.com>
2024-04-24 11:48:48 +0300

    made fix of console.table

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contactsData.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
       break;
 
     case "get":
